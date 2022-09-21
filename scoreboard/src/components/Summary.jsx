d3e5a0fcb9b0744e4c7547b11c2cb1307f02764f
jave92 <jave2310@gmail.com>
2022-09-21 12:33:57 +0200

    Improved summary style

--- scoreboard/src/components/Summary.jsx
+++ scoreboard/src/components/Summary.jsx
@@ -8,11 +8,15 @@ function Summary(props){
     const navigate = useNavigate();
     const summary = location.state.newArr;
     return(
-        <div>
-            <button onClick={() => navigate(-1)}>Back</button>
-            {summary.map( (game, index) => 
-                <p>{index+1}. {game.homename} {game.homescore} - {game.awayname} {game.awayscore}</p>
-            )}
+        <div className="summary-container">
+            <div className="summary-content">
+                {summary.map( (game, index) => 
+                    <p>{index+1}. {game.homename} {game.homescore} - {game.awayname} {game.awayscore}</p>
+                )}
+            </div>
+            <div className="summary-buttons-container">
+                <button className="btn-back" onClick={() => navigate(-1)}>Back</button>
+            </div>
         </div>
     );
 
